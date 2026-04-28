70ffe79f6a8d2d8dc12c294393727aa62bafbc44
Pawan Sharma <pawansharma@makeolix.com>
2026-04-28 10:53:52 +0530

    backend

---
+++ server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import multer from "multer";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const upload = multer({ dest: "uploads/" });
+app.use(cors());
+app.use(express.json());
+
+app.post("/send-mail", upload.single("resume"), async (req, res) => {
+  
+  const data = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+  user: process.env.EMAIL,
+  pass: process.env.PASS,
+},
+    });
+
+    // 🧠 smart detect (career vs business)
+    let emailContent = "";
+
+    if (data.companyName) {
+      // ✅ Business Form
+      emailContent = `
+--- Business Inquiry ---
+
+Company: ${data.companyName}
+Email: ${data.businessEmail}
+Phone: ${data.phone}
+Project Type: ${data.projectType}
+Budget: ${data.budget}
+Message: ${data.message}
+      `;
+    } else {
+      // ✅ Career Form
+      emailContent = `
+--- Job Application ---
+
+Name: ${data.fullName}
+Email: ${data.email}
+Phone: ${data.phone}
+Position: ${data.position}
+Experience: ${data.experience}
+Portfolio: ${data.portfolio}
+Message: ${data.message}
+      `;
+    }
+
+    await transporter.sendMail({
+      from: `"Website Form" <pawan@makeolix.com>`,
+      to: "pawan@makeolix.com",
+      subject: "New Form Submission 🚀",
+      text: emailContent,
+      attachments: req.file
+  ? [
+      {
+        filename: req.file.originalname,
+        path: req.file.path,
+      },
+    ]
+  : [],
+    });
+
+    res.json({ success: true });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false });
+  }
+});
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
