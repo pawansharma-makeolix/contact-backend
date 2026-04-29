dad9b57926ecee20105edfe7240368a91a7f82db
Pawan Sharma <pawansharma@makeolix.com>
2026-04-29 14:18:22 +0530

    back

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
 import multer from "multer";
-import https from "https"; 
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -87,21 +86,8 @@ Message:    ${data.message}
     res.status(500).json({ success: false, error: error.message });
   }
 });
-const https = require("https");
 
-setInterval(() => {
-  https.get("https://contact-backend-khx3.onrender.com/", (res) => {
-    console.log("Server jaag raha hai ✅", res.statusCode);
-  }).on("error", (err) => {
-    console.log("Ping error:", err.message);
-  });
-}, 5 * 60 * 1000); 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
