4a985da539e863499ec9c157d49268d7bc491e92
Pawan Sharma <pawansharma@makeolix.com>
2026-04-29 15:10:34 +0530

    ne

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 app.use(cors({
   origin:[ "https://makeolix.vercel.app",
    "http://localhost:5173",
-    "http://localhost:3000",]
+    "http://localhost:1000",]
 }));
 app.use(express.json());
 
